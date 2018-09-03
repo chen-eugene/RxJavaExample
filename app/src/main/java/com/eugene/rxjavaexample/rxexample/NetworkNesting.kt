34916d3eaf4bfb3c2d206954398386e0fa61d8b5
eugene <709519587@qq.com>
2018-09-03 09:12:05 +0800

    添加操作符

--- app/src/main/java/com/eugene/rxjavaexample/rxexample/NetworkNesting.kt
+++ app/src/main/java/com/eugene/rxjavaexample/rxexample/NetworkNesting.kt
@@ -34,7 +34,8 @@ class NetworkNesting {
         observable1.subscribeOn(Schedulers.io())               // （初始被观察者）切换到IO线程进行网络请求1
                 .observeOn(AndroidSchedulers.mainThread())  // （新观察者）切换到主线程 处理网络请求1的结果
                 .doOnNext({
-                    it.toString()
+                    Log.d(TAG, "第1次网络请求成功")
+                    Log.d(TAG, it.toString())
                 })
                 .observeOn(Schedulers.io())                 // （新被观察者，同时也是新观察者）切换到IO线程去发起登录请求
                 // 特别注意：因为flatMap是对初始被观察者作变换，所以对于旧被观察者，它是新观察者，所以通过observeOn切换线程
@@ -47,7 +48,7 @@ class NetworkNesting {
                 .observeOn(AndroidSchedulers.mainThread())  // （初始观察者）切换到主线程 处理网络请求2的结果
                 .subscribe({
                     Log.d(TAG, "第2次网络请求成功")
-                    it.toString()
+                    Log.d(TAG, it.toString())
                     // 对第2次网络请求返回的结果进行操作 = 显示翻译结果
                 }, {
                     Log.d(TAG, "登录失败")
